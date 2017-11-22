f38d38f7a4351ad14f9f35b64c641f8e9199b721
tonglar <glarexe@gmail.com>
2017-11-23 01:35:27 +0700

    origin

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-res.setHeader('Access-Control-Allow-Credentials', true);
+io.set('origins', '*:*');
+
 
 
 app.get('/chat', function(req, res){
